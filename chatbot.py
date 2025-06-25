f64424e01b82ab755d1972eb1764d78f8565b273
fitnessbadman <officialugobest@yahoo.com>
2025-06-25 16:55:53 +0800

    Add basic chatbot logic

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,9 @@
-print("Hello World")
+while True:
+    user_input = input("You: ").lower()
+    if "hello" in user_input:
+        print("Bot: Hi there! How can I help you?")
+    elif "bye" in user_input:
+        print("Bot: Goodbye! Have a great day.")
+        break
+    else:
+        print("Bot: Sorry, I didn’t understand that.")
